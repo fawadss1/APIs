afb302b59d7e0cc60541c999fd6888f8f71ea929
Fawad <fawadstar6@gmail.com>
2021-11-20 16:44:48 +0500

    First Update

--- App1/views.py
+++ App1/views.py
@@ -1,7 +1,5 @@
 from django.http import JsonResponse
-from rest_framework.response import responses
-from django.shortcuts import get_object_or_404
-from rest_framework import status
+from rest_framework.parsers import JSONParser
 from . import Serializer
 from . import models
 
@@ -11,5 +9,10 @@ def get(request):
         std = models.Student.objects.all()
         serializer = Serializer.StdSerliazer(std, many=True)
         return JsonResponse(serializer.data, safe=False)
-    if request.method == 'POST':
-        pass
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = Serializer.StdSerliazer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
