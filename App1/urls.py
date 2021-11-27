85688dfe26394678fe1e7b4cb54e1f5a0d8ea8b9
Fawad <fawadstar6@gmail.com>
2021-11-27 21:23:48 +0500

    Added New Model Classes

--- App1/urls.py
+++ App1/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="GET"),
+    path('', views.index, name="index"),
+    path('students', views.students, name="students"),
+    path('students/student<int:pk>', views.student_details, name="student Details"),
 ]
--- App1/views.py
+++ App1/views.py
@@ -1,6 +1,7 @@
 from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, JsonResponse
 from rest_framework.parsers import JSONParser
-from django.http import JsonResponse
+from rest_framework import status
 from . import Serializer
 from . import models
 
@@ -16,6 +17,14 @@ def index(request):
         instSerializer = Serializer.InstSerliazer(inst, many=True)
         var = stdSerializer.data, crsSerializer.data, instSerializer.data
         return JsonResponse(var, safe=False)
+
+
+@csrf_exempt
+def students(request):
+    if request.method == 'GET':
+        std = models.Student.objects.all()
+        stdSerializer = Serializer.StdSerliazer(std, many=True)
+        return JsonResponse(stdSerializer.data, safe=False)
     elif request.method == 'POST':
         data = JSONParser().parse(request)
         serializer = Serializer.StdSerliazer(data=data)
@@ -23,3 +32,14 @@ def index(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def student_details(request, pk):
+    try:
+        std = models.Student.objects.filter(pk=pk)
+    except models.Student.DoesNotExist:
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        stdSerializer = Serializer.StdSerliazer(std, many=True)
+        return JsonResponse(stdSerializer.data, safe=False)
